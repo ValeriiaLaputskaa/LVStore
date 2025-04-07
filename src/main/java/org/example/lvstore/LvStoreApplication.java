124bb08434f2a7e4c1877f54904fb1aabcff8a51
DESKTOP-914TGDE\valla <val.laputska@gmail.com>
2025-04-07 18:14:05 +0300

    feat: Initiate project on GitHub with Docker configuration

---
+++ src/main/java/org/example/lvstore/LvStoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.lvstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LvStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/lvstore/LvStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lvstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
